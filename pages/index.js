63f177e2336863d8f48b02bf1326cd648155b9cf
Daniel Goebel <dgoebel@uchicago.edu>
2022-07-08 13:12:24 -0500

    a first push to the repo

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>College Travel Group</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="College Travel Group" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        Join Columbia University, the University of Chicago, Vanderbilt University, and Brown to discover the exceptional opportunities our schools have to offer!</p>
+      <p>The college admissions process is an opportunity to reflect on your many options for the future. Whether you imagine yourself studying in a city, a suburb, or in a rural area; at a large, public university or a smaller, private school; or cheering on your favorite team or sleeping in on game day, you can learn about the features that will make your future college home a perfect fit for you! Admissions counselors will discuss:</p>
+
       </main>
 
       <Footer />
