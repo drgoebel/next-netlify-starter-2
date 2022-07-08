ecb1f70d6299691e7656c50a7ea166ecc0cd590e
Daniel Goebel <dgoebel@uchicago.edu>
2022-07-08 13:49:35 -0500

    small update to main page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="College Travel Group" />
         <p className="description">
-        Join Columbia University, the University of Chicago, Vanderbilt University, and Brown to discover the exceptional opportunities our schools have to offer!</p>
+        Join Columbia University, the University of Chicago, Vanderbilt University, and Brown to discover the exceptional college opportunities!</p>
       <p>The college admissions process is an opportunity to reflect on your many options for the future. Whether you imagine yourself studying in a city, a suburb, or in a rural area; at a large, public university or a smaller, private school; or cheering on your favorite team or sleeping in on game day, you can learn about the features that will make your future college home a perfect fit for you! Admissions counselors will discuss:</p>
 
       </main>
